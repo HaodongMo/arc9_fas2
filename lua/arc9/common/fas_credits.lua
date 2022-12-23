a0a02d63ac45392fac2ffcc8babec0cf1e68fc04
Haodong Mo <mohaodong@gmail.com>
2022-12-23 21:50:27 +1000

    Add some credits

---
+++ lua/arc9/common/fas_credits.lua
@@ -0,0 +1,24 @@
+--[[
+    Arctic - The guy that made this thing
+    FA:S Team - Most of the assets
+    Twinke Masta - Select assets
+    Tigg - Select assets
+    Krycek - Select assets
+    4Echo - Select assets
+    Millennia - Select assets
+    RevolutionArt - PEQ-15 model
+    eNse7en - Surefire model
+    tenoyl - DBAL-2
+    Urban Coalition - Select assets
+    MOR3 - TER optic, Romeo3
+    Farengar - Select assets
+    David Falke - M203
+    Havok101 - Glock 18 slide
+    EMDG - Glock 18 slide
+    ImBrokeRu - Select Assets
+    BuLL5h1t - Select Assets
+    Odeca - Select Assets
+    Sakura Sayaka - Select Assets
+    Rectus - Select Assets
+    MaxPresi - Select Assets
+]]
