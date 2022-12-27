312b9395186e105973fabeac1527e709be226848
Haodong Mo <mohaodong@gmail.com>
2022-12-27 14:03:08 +1000

    Unnecessary

--- lua/arc9/common/attachments/sticker_fas.lua
+++ lua/arc9/common/attachments/sticker_fas.lua
@@ -5,7 +5,5 @@ ATT.Icon = Material("stickers/fas_logo")
 ATT.SortOrder = 0
 ATT.Free = true
 
-ATT.Folder = "Stickers" -- Change this to point what part of the weapon it goes.
-
 ATT.Category = {"stickers"} -- Change this to point what weapon it is for.
 ATT.StickerMaterial = "stickers/fas_logo" -- Add a custom sticker image here.
